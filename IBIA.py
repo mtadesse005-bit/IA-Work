05b83ac09e87cb29d985445907928aabad21f2f8
mtadesse005-bit <mtadesse005@stpaul.k12.mn.us>
2026-01-12 12:37:41 -0600

    Update IBIA.py

--- IBIA.py
+++ IBIA.py
@@ -2,4 +2,25 @@ from kivymd.app import MDApp
 from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.screen import MDScreen
 
+class SimpleApp(MDApp):
+    def build(self):
+        screen = MDScreen()
+        
+        # A simple button in the middle
+        btn = MDRectangleFlatButton(
+            text="Click Me!",
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            on_release=self.change_text
+        )
+        
+        screen.add_widget(btn)
+        return screen
+
+    def change_text(self, button):
+        button.text = "It Worked!"
+        button.md_bg_color = (0, 1, 0, 1) # Changes to Green
+
+SimpleApp().run()
+
+
 
