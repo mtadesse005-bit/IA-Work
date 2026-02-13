ef38509fdc09dfb1ec27cbb07bbbfc6c510b41b5
mtadesse005-bit <mtadesse005@stpaul.k12.mn.us>
2026-02-13 14:57:16 -0600

    Update IBIA.py

--- IBIA.py
+++ IBIA.py
@@ -41,3 +41,47 @@ create_table()
 print("Database saved succesfully")                              
 
 
+def hash_password(hashed_password):
+   # Create a new sha256 hash object
+   sha_signature = hashlib.sha256(hashed_password.encode()).hexdigest()
+   return sha_signature
+
+
+
+
+
+def save_user(username_input, email_input, hashed_password):
+    conn = sqlite3.connect('ostofit.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+            INSERT INTO users (username_input, email_input, hashed_password)
+            VALUES (?, ?, ?)
+            ''', (username_input, email_input, hashed_password))
+    new_id = cursor.lastrowid
+    conn.commit()
+    return new_id
+
+
+
+def register(self):
+      username_input = self.ids.user_field.text
+      password_input = self.ids.password_field.text
+      confirm_password = self.ids.confirm_field.text
+      email_input = self.ids.email_field.text
+      if password_input == confirm_password:   
+          hashed_password = hash_password(confirm_password)
+          user_id = save_user(username_input, email_input, hashed_password)
+          MDApp.get_running_app().current_user_id = user_id
+      else:
+          print("Passwords do not match")
+
+
+
+
+
+
+
+#register() 
+#save_user("Moti", "motimod223@gmail.com", "730459")    
+
+      
