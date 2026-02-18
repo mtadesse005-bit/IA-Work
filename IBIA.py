945643face45b5a12afe0cb5ae4616958ea4940a
mtadesse005-bit <mtadesse005@stpaul.k12.mn.us>
2026-02-18 14:56:52 -0600

    Update IBIA.py

--- IBIA.py
+++ IBIA.py
@@ -38,7 +38,79 @@ def create_table():
    conn.close()
 
 create_table()
-print("Database saved succesfully")                              
+print("Database saved succesfully")   
+
+
+
+KV = '''
+ScreenManager:
+    LoginScreen:
+    RegisterScreen:
+
+<LoginScreen>:
+    name: 'login'
+    MDBoxLayout:
+        orientation: 'vertical'
+        padding: "20dp"
+        spacing: "10dp"
+        MDLabel:
+            text: "Ostofit Login"
+            halign: "center"
+            font_style: "H4"
+        MDTextField:
+            id: login_email
+            hint_text: "Email"
+        MDTextField:
+            id: login_password
+            hint_text: "Password"
+            password: True
+        MDRaisedButton:
+            text: "Login"
+            pos_hint: {"center_x": .5}
+        MDFlatButton:
+            text: "Don't have an account? Register"
+            pos_hint: {"center_x": .5}
+            on_release: root.login_user()
+
+<RegisterScreen>:
+    name: 'register'
+    MDBoxLayout:
+        orientation: 'vertical'
+        padding: "20dp"
+        spacing: "10dp"
+        MDLabel:
+            text: "Create Account"
+            halign: "center"
+            font_style: "H4"
+        MDTextField:
+            id: user_field
+            hint_text: "Username"
+        MDTextField:
+            id: email_field
+            hint_text: "Email"
+        MDTextField:
+            id: password_field
+            hint_text: "Password"
+            password: True
+        MDTextField:
+            id: confirm_field
+            hint_text: "Confirm Password"
+            password: True
+        MDRaisedButton:
+            text: "Register"
+            pos_hint: {"center_x": .5}
+            on_release: root.register_user() # Matches method name below
+        MDFlatButton:
+            text: "Back to Login"
+            pos_hint: {"center_x": .5}
+            on_release: root.manager.current = 'login'
+'''
+
+
+
+
+
+
 
 
 def hash_password(hashed_password):
@@ -59,21 +131,57 @@ def save_user(username_input, email_input, hashed_password):
             ''', (username_input, email_input, hashed_password))
     new_id = cursor.lastrowid
     conn.commit()
+    conn.close()
     return new_id
 
 
+class LoginScreen(Screen):
+   def login_user(self):
+    email = self.ids.email_login.text
+    password = self.ids.password_login.text
+    if password == "" or email == "":
+       print("Fill out all fields")
+       return
+    
+    hashed_attempt = hash_password(password)
 
-def register(self):
+    conn = sqlite3.connect('ostofit.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM users WHERE email_input = ? AND hashed_password = ?", (email, hashed_attempt))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user:
+       MDApp.get_running_app().current_user_id = user[0]
+       print(f"Login Successful User ID: {user[0]}")
+    else:
+       print("Invaild User Info")
+       
+
+
+
+class RegisterScreen(Screen):
+   def register_user(self):
       username_input = self.ids.user_field.text
       password_input = self.ids.password_field.text
       confirm_password = self.ids.confirm_field.text
       email_input = self.ids.email_field.text
-      if password_input == confirm_password:   
+      if password_input == confirm_password and password_input != "":   
           hashed_password = hash_password(confirm_password)
           user_id = save_user(username_input, email_input, hashed_password)
           MDApp.get_running_app().current_user_id = user_id
+          print(f"Account created successfully, {user_id}")
       else:
           print("Passwords do not match")
+
+
+class OstofitApp(MDApp):
+   current_user_id = None
+
+   def build(self):
+      return Builder.load_string(KV)
+if __name__ == '__main__':
+   OstofitApp().run()
 
 
 
@@ -84,4 +192,6 @@ def register(self):
 #register() 
 #save_user("Moti", "motimod223@gmail.com", "730459")    
 
-      
+
+
+#create_table()     
